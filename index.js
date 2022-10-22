ad4129bba085729d9ce73315a097354190c961d1
Matthew Starcher <mlstarcher@gmail.com>
2022-10-22 11:22:45 -0700

    Successfully tested using external midi device over usb (mpd218)

--- index.js
+++ index.js
@@ -4,7 +4,12 @@ let inputs = easymidi.getInputs();
 
 console.log('Inputs: ', inputs);
 
-let iac = new easymidi.Input('IAC Driver Bus 1');
-iac.on('noteon', (msg) => {
-  console.log('msg: ', msg);
+// let iac = new easymidi.Input('IAC Driver Bus 1');
+// iac.on('noteon', (msg) => {
+//   console.log('msg: ', msg);
+// })
+
+let mpd = new easymidi.Input('MPD218 Port A');
+mpd.on('noteon', (msg) => {
+  console.log('John should stop touring!');
 })
